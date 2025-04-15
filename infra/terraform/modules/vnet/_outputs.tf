b0f6dd57315d9c45fe9d042fdc812c62262d82b8
Shige Fukushima <shigeyf@outlook.com>
2025-04-15 15:48:40 +0900

    fix: 🐛 Fixed a bug for output

--- infra/terraform/modules/vnet/_outputs.tf
+++ infra/terraform/modules/vnet/_outputs.tf
@@ -6,9 +6,9 @@ output "output" {
     vnet_id    = azurerm_virtual_network.this.id
     subnet_ids = { for key, subnet in azurerm_subnet.this : key => subnet.id }
     nsg_ids    = { for key, nsg in azurerm_network_security_group.this : key => nsg.id }
-    bastion    = bastion.output
-    nat_gw     = natgw.output
-    vpn_gw     = vpngw.output
+    bastion    = var.enable_bastion_host ? module.bastion[0].output : null
+    nat_gw     = var.enable_nat_gateway ? module.natgw[0].output : null
+    vpn_gw     = var.enable_vpn_gateway ? module.vpngw[0].output : null
   }
   description = "Ids for Virtual Nwetwork resources"
 }
